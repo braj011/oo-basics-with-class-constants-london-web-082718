541a99fbb4168805ec82c6fba3c3a197550120fa
Bhagya Raj <bhagyaraj01@gmail.com>
2018-08-30 15:58:17 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,14 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
   BRANDS = []
   
   def initialize(brand)
     @brand = brand
     
-    #BRANDS << brand unless BRANDS.include?(brand)
-    BRANDS << brand if (!BRANDS.include?(brand))
+    MANY WAYS TO WRITE - HERE ARE 2 WAYS
+    # 1. BRANDS << brand unless BRANDS.include?(brand)
+    # 2. BRANDS << brand if (!BRANDS.include?(brand))
     
   end
 
